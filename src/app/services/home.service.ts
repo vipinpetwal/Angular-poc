b3fed6eed8f4f53fc698b9a080ce6ed5fc5ff193
vipinpetwal <48955607+vipinpetwal@users.noreply.github.com>
2019-07-26 19:32:02 +0530

    Add files via upload

---
+++ src/app/services/home.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable()
+export class HomeService {
+
+  constructor(private httpClient: HttpClient) { }
+
+
+  getrecords()
+  {
+    const displaydata = [{ "name": "TestRLSWell01", "type": "rls", "source": "1000001" }, { "name": "TestESPWell01", "type": "esp", "source": "1000002" }, { "name": "TestRLSWell01", "type": "rls", "source": "1000003" }];
+    
+    return displaydata;
+  }
+
+
+  getemprecord()
+  {
+    console.log("payload9++++++++++++")
+    return this.httpClient.get('api/v1/displaydata');
+  }
+
+}
