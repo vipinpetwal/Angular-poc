c1981745cdc80c3c71184f10dcbc3c8203d1a0de
vipinpetwal <48955607+vipinpetwal@users.noreply.github.com>
2019-08-01 19:55:11 +0530

    Update app.routing.ts

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -5,13 +5,15 @@ import { WelllistComponent } from './components/welllist/welllist.component';
 import { LoginComponent } from './components/login/login.component';
 import { AuthGuard } from './auth.guard';
 import { DragdropComponent } from './components/dragdrop/dragdrop.component';
+import { SearchComponent } from './components/search/search.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'login' },
-  { path: 'login', component: LoginComponent, pathMatch: 'full'},
+  { path: 'login', component: LoginComponent, pathMatch: 'full' },
   //{ path:'home',component: FirstcompComponent , canActivate:[AuthGuard]},
   //{ path:'home',component: WelllistComponent , canActivate:[AuthGuard]},DragdropComponent
-  { path:'home',component: DragdropComponent , canActivate:[AuthGuard]},
+  //{ path:'home',component: DragdropComponent , canActivate:[AuthGuard]},
+  { path: 'home', component: SearchComponent, canActivate: [AuthGuard] },
 
 ];
 @NgModule({
